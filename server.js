6c874ed0860ece66f53a6312470f7e74b7de7fc0
Muhammadosama007 <115362575+Muhammadosama007@users.noreply.github.com>
2024-09-01 18:07:07 +0500

    added status(404)

--- server.js
+++ server.js
@@ -9,41 +9,91 @@ mongoose.connect('mongodb://localhost:27017/books');
 
 app.post('/books/create', (req, res) => {
     const book = new Book(req.body);
-    book.save();
-    res.send(book);
+    if (!book) {
+        res.status(404).send({
+            message: "Book Not Created"
+        })
+    }
+    else {
+        book.save();
+        res.send(book);
+    }
+
 })
 
 app.get('/books/get', async (req, res) => {
     const bookData = await Book.find();
-    // console.log("books data: ",bookData);
-    res.send(bookData);
+    if (!bookData) {
+        res.status(404).send({
+            message: "Books Not found!!!"
+        })
+    }
+    else {
+        // console.log("books data: ",bookData);
+        res.send(bookData);
+    }
 })
 
 app.get('/booksByName', async (req, res) => {
     const bookByName = await Book.findOne({ title: req.query.title })
-    res.send(bookByName)
+    if (!bookByName) {
+        res.status(404).send({
+            message: "Can't get book by Title!!!"
+        })
+    }
+    else {
+        res.send(bookByName)
+    }
 })
 
 app.get('/booksByAuthor', async (req, res) => {
     const booByAuthor = await Book.findOne({ author: req.query.author })
-    res.send(booByAuthor);
+    if (!booByAuthor) {
+        res.status(404).send({
+            message: "Can't get book by Author!!!"
+        })
+    }
+    else {
+        res.send(booByAuthor);
+    }
 })
 
 app.get('/booksById/:id', async (req, res) => {
     const bookById = await Book.findById(req.params.id);
-    res.send(bookById);
+    if (!bookById) {
+        res.status(404).send({
+            message: "Can't get book by id!!!"
+        })
+    }
+    else {
+        res.send(bookById);
+    }
 })
 
 app.patch('/books/Update/:id', async (req, res) => {
     const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body);
-    updateBook.save();
-    res.send(updateBook);
+    if (!updateBook) {
+        res.status(404).send({
+            message: "Can't Update book!!!"
+        })
+    }
+    else {
+        updateBook.save();
+        res.send(updateBook);
+    }
 })
 
 app.delete('/books/delete/:id', async (req, res) => {
     const indexDel = await Book.findByIdAndDelete(req.params.id);
-    indexDel.save();
-    res.send(indexDel);
+    if (!indexDel) {
+        res.status(404).send({
+            message: "Can't Delete book!!!"
+        })
+    }
+    else {
+        indexDel.save();
+        res.send(indexDel);
+    }
 })
 
 const port = 3000;
